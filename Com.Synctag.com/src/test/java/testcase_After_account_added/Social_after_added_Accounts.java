a86cbd74e11bd403297596f879a4869a2303cb41
tpk-synctag <thangaprathap.synctag@gmail.com>
2018-07-17 14:32:23 +0530

    Exception implemented for all user mode

--- Com.Synctag.com/src/test/java/testcase_After_account_added/Social_after_added_Accounts.java
+++ Com.Synctag.com/src/test/java/testcase_After_account_added/Social_after_added_Accounts.java
@@ -1,7 +1,5 @@
 package testcase_After_account_added;
 
-import java.util.Queue;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
@@ -52,96 +50,164 @@ public class Social_after_added_Accounts
 	
 	
 	@Test
-	public void socialsynctag() throws Exception 
+	public void socialsynctag() 
 	{
 		
 		
-			HomePages pag=PageFactory.initElements(driver, HomePages.class);
-			pag.homepages_loginbutton();
-					
-			 //Login to synctag Account
+			try {
+				
+				HomePages pag=PageFactory.initElements(driver, HomePages.class);
+				pag.homepages_loginbutton();
+						
+				 //Login to synctag Account
+				
+				Thread.sleep(2000);
+				LoginPage log=PageFactory.initElements(driver, LoginPage.class);
+				Thread.sleep(5000);			
+				log.clickloginbutton(DataProviderFactory.getexcel().getdata(0, 0, 0),DataProviderFactory.getexcel().getdata(0, 0, 1));
+				test.log(LogStatus.PASS, "----------- Synctag Login successfully ------------------");
+				
+				Thread.sleep(10000);
+			} 
+			catch (Exception e) 
 			
-			Thread.sleep(2000);
-			LoginPage log=PageFactory.initElements(driver, LoginPage.class);
-			Thread.sleep(5000);			
-			log.clickloginbutton(DataProviderFactory.getexcel().getdata(0, 0, 0),DataProviderFactory.getexcel().getdata(0, 0, 1));
-			test.log(LogStatus.PASS, "----------- Synctag Login successfully ------------------");
-			
-			Thread.sleep(10000);
+			{
+				System.out.println("The Home page Exception is :" +e.getMessage());
+				
+			}
 			
 			// Home Feeds 
 			
-			/*Social_Filter fil=PageFactory.initElements(driver, Social_Filter.class );
-			
-			fil.timeline_to_home();		
-					
-			fil.click_filters();
-			
-			Thread.sleep(10000);
-			Social_AddAcount socil=PageFactory.initElements(driver, Social_AddAcount.class );
-			
-			socil.click_home_Fbfilter();
-			fil.click_filters();
-			
-			socil.click_home_twetfilter();
-			fil.click_filters();
-			
-			socil.click_home_synclogo();*/
+			try {
+				Social_Filter fil=PageFactory.initElements(driver, Social_Filter.class );
+				
+				fil.timeline_to_home();		
+						
+				fil.click_filters();
+				
+				Thread.sleep(10000);
+				Social_AddAcount socil=PageFactory.initElements(driver, Social_AddAcount.class );
+				
+				socil.click_home_Fbfilter();
+				fil.click_filters();
+				
+				socil.click_home_twetfilter();
+				fil.click_filters();
+				
+				socil.click_home_synclogo();
+				
+			}
+			catch (Exception e)
+			{
+				System.out.println("The Home feed Exception is :"+e.getMessage());		
+				
+			}
 			
 			
 			// Friends and Followers 
 			
-			/*Friends_And_Followers frnd_follow=PageFactory.initElements(driver, Friends_And_Followers.class );
-			frnd_follow.click_friends_Followers();
-			Thread.sleep(5000);
-			frnd_follow.frineds_and_Followers_facebook();
-			Thread.sleep(5000);
-			frnd_follow.click_friends_Followers();
-			Thread.sleep(9000);
-			frnd_follow.friends_and_followers_twitter();
-			Thread.sleep(5000);
-			frnd_follow.twitter_syncpage_filter();
-			Thread.sleep(5000);*/
+			try {
+				Friends_And_Followers frnd_follow=PageFactory.initElements(driver, Friends_And_Followers.class );
+				frnd_follow.click_friends_Followers();
+				Thread.sleep(5000);
+				frnd_follow.frineds_and_Followers_facebook();
+				Thread.sleep(5000);
+				frnd_follow.click_friends_Followers();
+				Thread.sleep(9000);
+				frnd_follow.friends_and_followers_twitter();
+				Thread.sleep(5000);
+				frnd_follow.twitter_syncpage_filter();
+				Thread.sleep(5000);
+			} 
+			catch (Exception e)
+			{
+				System.out.println("The Frineds and follower Exception is :"+e.getMessage());	
+				
+			}
 			
 			
 			// Time Line for User mode
 			
-			/*Time_Line_Feeds timeline=PageFactory.initElements(driver, Time_Line_Feeds.class);
-			timeline.filter_time_line();
+			try 
+			{
+				Time_Line_Feeds timeline=PageFactory.initElements(driver, Time_Line_Feeds.class);
+				timeline.filter_time_line();
+				
+				timeline.timeline_facebook();
+				timeline.filter_time_line();
+				Thread.sleep(5000);
+				
+				timeline.timeline_twitter();
+				timeline.filter_time_line();
+				Thread.sleep(5000);
+				
+				timeline.timeline_instagram();
+				timeline.filter_instgram_time_line();
+				Thread.sleep(5000);
 			
-			timeline.timeline_facebook();
-			timeline.filter_time_line();
-			Thread.sleep(5000);
+			}
+			catch (Exception e) 
+			{
+				System.out.println("The TimeLine Exception is :"+e.getMessage());
+			}
 			
-			timeline.timeline_twitter();
-			timeline.filter_time_line();
-			Thread.sleep(5000);
-			
-			timeline.timeline_instagram();
-			timeline.filter_instgram_time_line();
-			Thread.sleep(5000);
-			*/
 				
 			// Queue Menu 
 			
-			/*Queue_Menu ques=PageFactory.initElements(driver, Queue_Menu.class);
-			ques.click_Queuemenu();
-			Thread.sleep(5000);
-			ques.queue_facebook();
-			Thread.sleep(5000);
-			ques.queue_twitter();
-			Thread.sleep(5000);*/
-			
-			/*MailBox mailbox=PageFactory.initElements(driver, MailBox.class);
-			mailbox.mailbox();
-			Thread.sleep(5000);
-			*/
+			try {
+				Queue_Menu ques=PageFactory.initElements(driver, Queue_Menu.class);
+				ques.click_Queuemenu();
+				Thread.sleep(5000);
+				ques.queue_facebook();
+				Thread.sleep(5000);
+				ques.queue_twitter();
+				Thread.sleep(5000);
+			}
+			catch (Exception e) 
+			{
 				
-			/*Mentions mention=PageFactory.initElements(driver, Mentions.class);
-			mention.mentionfunction();*/
+				System.out.println("The Queue Exception is :"+e.getMessage());
+				
+			}
 			
-			Archive arch=PageFactory.initElements(driver, Archive.class);
-			arch.archivefunctions();
+			// MailBox 
+			try {
+				MailBox mailbox=PageFactory.initElements(driver, MailBox.class);
+				mailbox.mailbox();
+				Thread.sleep(5000);
+			}
+			catch (Exception e) 
+			{
+				System.out.println("The MailBox Excetion is :"+e.getMessage());
+				
+			}
+			
+			//Mentions	
+			
+			try
+			{
+				
+				Mentions mention=PageFactory.initElements(driver, Mentions.class);
+				mention.mentionfunction();
+			}
+			catch (Exception e) 
+			{
+				System.out.println("The Mention Exceptio is :"+e.getMessage());
+			}
+			
+			//Archive 
+			
+			try 
+			{
+			
+				Archive arch=PageFactory.initElements(driver, Archive.class);
+				arch.archivefunctions();
+				
+			} 
+			catch (Exception e) 
+			{
+				System.out.println("The Archive Exception is :"+e.getMessage());
+			}
 			
 			
 	}
